5e2bb0e666827328790feea170c0b33b3361a7d5
Dmytro Ben <dmitriy.ben@gmail.com>
2021-07-03 19:05:21 +0300

    feat(redux): added alert on fetch posts error

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -16,15 +16,20 @@ export function createPost(post) {
 
 export function fetchPosts() {
   return async dispatch => {
-    dispatch(showLoader())
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-    const json = await response.json()
-    dispatch(hideLoader())
+    try {
+      dispatch(showLoader())
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      const json = await response.json()
+      dispatch(hideLoader())
 
-    dispatch({
-      type: FETCH_POSTS,
-      payload: json
-    })
+      dispatch({
+        type: FETCH_POSTS,
+        payload: json
+      })
+    } catch (e) {
+      dispatch(showAlert('Internal error'))
+      dispatch(hideLoader())
+    }
   }
 }
 
